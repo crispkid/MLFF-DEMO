901793c6005d86f5e14592092f7bd7250fe0e660
Peter <crispkid@gmail.com>
2018-10-21 14:52:58 +0800

    Update Controllers/BalanceController.cs

---
+++ Controllers/BalanceController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+using Database;
+
+namespace MLFF_DEMO.Controllers {
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class BalanceController : ControllerBase {
+
+        //SQL Connetion String
+        //Database.Database myDatabase = new Database.Database();
+        string sqlcommstring = Database.Database.SqlConnationString;
+
+        // GET api/values
+        [HttpGet("{USER_SN}")]
+        public ActionResult<IEnumerable<float>> Get (string USER_SN) {
+            IEnumerable<float> myBalance = null;
+            using (var cn = new SqlConnection (sqlcommstring)) {
+                cn.Open ();
+                string sql = @"SELECT TOP 1
+                                    BALANCE
+                                FROM MLFF_DB..Virtual_Account WITH (NOLOCK)
+                                WHERE USER_SN=@USER_SN
+                                ORDER BY TRANSACTION_Time DESC;";
+                myBalance = cn.Query<float> (sql);
+            }
+ 
+            return Ok(myBalance);
+        }
+    }
+}
