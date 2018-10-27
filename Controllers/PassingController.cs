c1517f375be83ff5a426e08e668cd3c1dc7ce0a2
Peter <crispkid@gmail.com>
2018-10-27 18:39:46 +0800

    fix passing sql 3st

--- Controllers/PassingController.cs
+++ Controllers/PassingController.cs
@@ -22,23 +22,24 @@ namespace MLFF_DEMO.Controllers {
             using (var cn = new SqlConnection (sqlcommstring)) {
                 cn.Open ();
                 string sql = @"DECLARE @HIS_SN VARCHAR(100), @USER_SN VARCHAR(100) , @TRANSACTION_SN VARCHAR(100), @AMOUNT FLOAT, @BALANCE FLOAT, @PASSING_TIME DATETIME;
-                            SELECT @USER_SN=USER_SN FROM MLFF_DB..USERS WHERE EPC=@EPC;
-                            SELECT TOP 1 @PASSING_TIME=PASSING_TIME FROM MLFF_DB..Passing_His 
+                            SELECT @USER_SN=USER_SN FROM MLFF_DB..USERS WITH (NOLOCK) WHERE EPC=@EPC;
+                            SELECT TOP 1 @PASSING_TIME=PASSING_TIME FROM MLFF_DB..Passing_His WITH (NOLOCK)
                             WHERE USER_SN=@USER_SN AND GANTRY_SN=@GANTRY_SN
                             ORDER BY PASSING_TIME DESC;
-                            IF DATEADD(SECOND, 10, MLFF_DB.dbo.GetLocalDate(DEFAULT))>ISNULL(@PASSING_TIME, MLFF_DB.dbo.GetLocalDate(DEFAULT))
+                            IF DATEADD(SECOND, -10, MLFF_DB.dbo.GetLocalDate(DEFAULT))>@PASSING_TIME
+                                OR @PASSING_TIME IS NULL
                             BEGIN
                                 SELECT @HIS_SN='PSHS'+CONVERT(VARCHAR(8), GETDATE(), 112)+CONVERT(VARCHAR(10), COUNT(HIS_SN)+1)
-                                FROM MLFF_DB..Passing_His;
+                                FROM MLFF_DB..Passing_His WITH (NOLOCK);
                                 SELECT @TRANSACTION_SN='TSN'+CONVERT(VARCHAR(8), GETDATE(), 112)+CONVERT(VARCHAR(10), COUNT(TRANSACTION_SN)+1)
-                                FROM MLFF_DB..Virtual_Account;
+                                FROM MLFF_DB..Virtual_Account WITH (NOLOCK);
                                 SELECT @AMOUNT=GANTRY_FEE
-                                FROM MLFF_DB..Gantrys
+                                FROM MLFF_DB..Gantrys WITH (NOLOCK)
                                 WHERE GANTRY_SN=@GANTRY_SN;
                                 INSERT INTO Passing_His
                                     (HIS_SN, USER_SN, GANTRY_SN, PASSING_TIME)
                                 VALUES(@HIS_SN, @USER_SN, @GANTRY_SN, MLFF_DB.dbo.GetLocalDate(DEFAULT));
-                                SELECT TOP 1 @BALANCE=BALANCE FROM MLFF_DB..Virtual_Account WHERE USER_SN=@USER_SN ORDER BY TRANSACTION_TIME DESC;
+                                SELECT TOP 1 @BALANCE=BALANCE FROM MLFF_DB..Virtual_Account WITH (NOLOCK) WHERE USER_SN=@USER_SN ORDER BY TRANSACTION_TIME DESC;
                                 SET @BALANCE=@BALANCE-@AMOUNT;
                                 INSERT INTO MLFF_DB..Virtual_Account
                                     (TRANSACTION_SN, USER_SN, TRANSACTION_TYPE, AMOUNT, BALANCE, TRANSACTION_TIME, DESCRIPTION_NOTE)
